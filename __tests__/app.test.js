bed8f999d71c893ecede5376b243c97ea22cedfa
jamezharvey <jamesharvey1080@gmail.com>
2022-03-28 16:50:50 +0100

    completed get req task

---
+++ __tests__/app.test.js
@@ -0,0 +1,34 @@
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const request = require("supertest");
+const app = require("../app");
+
+afterAll(() => db.end());
+beforeEach(() => seed(testData));
+
+describe("GET /api/topics", () => {
+  test("200: responds with all topics", () => {
+    const expected = [
+      {
+        description: "The man, the Mitch, the legend",
+        slug: "mitch",
+      },
+      {
+        description: "Not dogs",
+        slug: "cats",
+      },
+      {
+        description: "what books are made of",
+        slug: "paper",
+      },
+    ];
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.topics).toBeInstanceOf(Array);
+        expect(res.body.topics).toEqual(expected);
+      });
+  });
+});
