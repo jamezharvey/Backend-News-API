38029ebb0ddca71ebaa02f42c6e77d62da8c7e91
jamezharvey <jamesharvey1080@gmail.com>
2022-03-29 11:22:35 +0100

    added test for 404 error

---
+++ __tests__/app.test.js
@@ -0,0 +1,42 @@
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const request = require("supertest");
+const app = require("../app");
+
+afterAll(() => db.end());
+beforeEach(() => seed(testData));
+
+describe("GET /api/topics", () => {
+  test("200: responds with all topics", () => {
+    const expected = [
+      {
+        description: "The man, the Mitch, the legend",
+        slug: "mitch",
+      },
+      {
+        description: "Not dogs",
+        slug: "cats",
+      },
+      {
+        description: "what books are made of",
+        slug: "paper",
+      },
+    ];
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.topics).toBeInstanceOf(Array);
+        expect(res.body.topics).toEqual(expected);
+      });
+  });
+  test("404: bad request", () => {
+    return request(app)
+      .get("/api/topicos") // spelled wrong, bad request
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
+/* app.use((err, req, res, next) => {
+  const badReqCodes = ["42703", "22P02"];
+  if (badReqCodes.includes(err.code)) {
+    res.status(400).send({ msg: "bad request" });
+  } else {
+    next(err);
+  }
+});
+*/
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
 });
